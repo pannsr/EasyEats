912d698ff91bc412fb7295bed77bd6094f94fe30
TalayKD <talaykd@gmail.com>
2022-07-09 17:04:20 +0700

    layout branch + navigation

--- src/screens/Branch.tsx
+++ src/screens/Branch.tsx
@@ -3,6 +3,7 @@ import {FlatList} from 'react-native';
 
 import {useData, useTheme, useTranslation} from '../hooks/';
 import {Block, Button, Image, Input, BranchComponent, Modal, Text} from '../components/';
+import { useNavigation } from '@react-navigation/native';
 
 const Branch = () => {
   const {t} = useTranslation();
@@ -10,6 +11,7 @@ const Branch = () => {
   const {branches} = useData();
   const [products, setProducts] = useState(branches);
   const {assets, colors, fonts, gradients, sizes} = useTheme();
+  const navigation = useNavigation();
   
 
   /* initializing variables for branch drop-down menu */
@@ -21,102 +23,111 @@ const Branch = () => {
   const [tableAt, setTable] = useState(1);
 
   return (
-    <Block>
-      {/* products list */}
       <Block
         scroll
         paddingHorizontal={sizes.padding}
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{paddingBottom: sizes.l}}>
-        <Text h4 marginVertical={sizes.s} marginTop={sizes.sm}>
-          Choose a branch for this restaurant
-        </Text>
-        <Button
-            marginTop={sizes.xs}
-            flex={1}
-            row
-            gradient={gradients.dark}
-            onPress={() => setModal(true)}>
-            <Block
-              row
-              align="center"
-              justify="space-between"
-              paddingHorizontal={sizes.sm}>
-              <Text white bold transform="uppercase" marginRight={sizes.sm}>
-                {branchAt.title}
-              </Text>
-              <Image
-                source={assets.arrow}
-                color={colors.white}
-                transform={[{rotate: '90deg'}]}
+        contentContainerStyle={{paddingBottom: sizes.l, alignItems: 'center'}}>
+          <Block contentContainerStyle={{flex:1}}>
+            <Text h4 marginVertical={sizes.s} marginTop={sizes.sm}>
+              Choose a branch for this restaurant
+            </Text>
+            <Button
+                marginTop={sizes.xs}
+                flex={1}
+                row
+                gradient={gradients.dark}
+                onPress={() => setModal(true)}>
+                <Block
+                  row
+                  align="center"
+                  justify="space-between"
+                  paddingHorizontal={sizes.sm}>
+                  <Text white bold transform="uppercase" marginRight={sizes.sm}>
+                    {branchAt.title}
+                  </Text>
+                  <Image
+                    source={assets.arrow}
+                    color={colors.white}
+                    transform={[{rotate: '90deg'}]}
+                  />
+                </Block>
+            </Button>
+            <Modal visible={showModal} onRequestClose={() => setModal(false)}>
+              <FlatList
+                data={branchObj}
+                renderItem={({item}) => (
+                  <Button
+                    marginBottom={sizes.sm}
+                    onPress={() => {
+                      setBranch(item);
+                      setModal(false);
+                    }}>
+                    <Text p white semibold transform="uppercase">
+                      {item.title}
+                    </Text>
+                  </Button>
+                )}
               />
-            </Block>
-        </Button>
-        <Modal visible={showModal} onRequestClose={() => setModal(false)}>
-          <FlatList
-            data={branchObj}
-            renderItem={({item}) => (
-              <Button
-                marginBottom={sizes.sm}
-                onPress={() => {
-                  setBranch(item);
-                  setModal(false);
-                }}>
-                <Text p white semibold transform="uppercase">
-                  {item.title}
-                </Text>
-              </Button>
-            )}
-          />
-        </Modal>
+            </Modal>
 
-        {/* table input number */}
-        <Text h4 marginVertical={sizes.s} marginTop={sizes.l}>
-          Input your table number
-        </Text>
-        <Button
-            marginTop={sizes.xs}
-            flex={1}
-            row
-            gradient={gradients.dark}
-            onPress={() => setModalTable(true)}>
-            <Block
-              row
-              align="center"
-              justify="space-between"
-              paddingHorizontal={sizes.sm}>
-              <Text white bold transform="uppercase" marginRight={sizes.sm}>
-                {tableAt}
-              </Text>
-              <Image
-                source={assets.arrow}
-                color={colors.white}
-                transform={[{rotate: '90deg'}]}
+            {/* table input number */}
+            <Text h4 marginVertical={sizes.s} marginTop={sizes.l}>
+              Input your table number
+            </Text>
+            <Button
+                marginTop={sizes.xs}
+                flex={1}
+                row
+                gradient={gradients.dark}
+                onPress={() => setModalTable(true)}>
+                <Block
+                  row
+                  align="center"
+                  justify="space-between"
+                  paddingHorizontal={sizes.sm}>
+                  <Text white bold transform="uppercase" marginRight={sizes.sm}>
+                    {tableAt}
+                  </Text>
+                  <Image
+                    source={assets.arrow}
+                    color={colors.white}
+                    transform={[{rotate: '90deg'}]}
+                  />
+                </Block>
+            </Button>
+            <Modal visible={showModalTable} onRequestClose={() => setModalTable(false)}>
+              <FlatList
+                keyExtractor={(index) => `${index}`}
+                data={Array.from(Array(branchAt.numTable).keys()).map(x => x+1)}
+                renderItem={({item}) => (
+                  <Button
+                    marginBottom={sizes.sm}
+                    onPress={() => {
+                      setTable(item);
+                      setModalTable(false);
+                    }}>
+                    <Text p white semibold transform="uppercase">
+                      {item}
+                    </Text>
+                  </Button>
+                )}
               />
-            </Block>
-        </Button>
-        <Modal visible={showModalTable} onRequestClose={() => setModalTable(false)}>
-          <FlatList
-            keyExtractor={(index) => `${index}`}
-            data={Array.from(Array(branchAt.numTable).keys()).map(x => x+1)}
-            renderItem={({item}) => (
-              <Button
-                marginBottom={sizes.sm}
-                onPress={() => {
-                  setTable(item);
-                  setModal(false);
-                }}>
-                <Text p white semibold transform="uppercase">
-                  {item}
+            </Modal>
+          </Block>
+          <Block>
+            <Button 
+            onPress={() => navigation.navigate('Screens', {screen: 'MenuPage'})}
+            gradient={gradients.success} 
+            marginVertical={sizes.xl} 
+            rounded={true} 
+            height={30} width={300}>
+                <Text white bold transform="uppercase">
+                  Next
                 </Text>
-              </Button>
-            )}
-          />
-        </Modal>
-
-        
+            </Button>
+          </Block>    
       </Block>
-    </Block>
   );
 };
 
